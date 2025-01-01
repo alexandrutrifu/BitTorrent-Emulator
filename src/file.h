0c7da35b61ad81e446f83e2a24d7356e63266d38
Alexandru Trifu-Urzica <alex@Alexandrus-MacBook-Pro.local>
2025-01-01 14:26:43 +0200

    overrode ==

--- src/file.h
+++ src/file.h
@@ -5,11 +5,29 @@
 using namespace std;
 
 class File {
+    private:
+        enum FileStatus {
+            INCOMPLETE,
+            COMPLETE
+        };
+
     public:
         File() = default;
+        File(string name) { this->name = name; }
         ~File() = default;
+
+        void completeStatus() { status = COMPLETE; }
+
+        bool operator==(const File &file) const {
+            return name == file.name;
+        }
+        bool operator!=(const File &file) const {
+            return name != file.name;
+        }
 
         string name;
         int segmentCount;
+        FileStatus status = INCOMPLETE;
+
         unordered_map<int, string> segments;    // (segmentIndex, segmentHash)
 };
