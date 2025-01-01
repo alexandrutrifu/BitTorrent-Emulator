f2fe332f19ca54abf1cc4fb9559932e0c9037914
Alexandru Trifu-Urzica <alex@Alexandrus-MacBook-Pro.local>
2025-01-01 20:06:36 +0200

    added common object header

--- src/common.h
+++ src/common.h
@@ -5,3 +5,45 @@
 #define MAX_CHUNKS 100
 
 #define SEGMENT_SIZE 33
+
+enum ClientRequest {
+    REQUEST_SEEDS,
+    DOWNLOAD_COMPLETE,
+    FINISHED,
+    UPDATE_SWARM
+};
+
+enum ClientStatus {
+    SEED,
+    DONE
+};
+
+int requestIndex(ClientRequest request) {
+    switch (request) {
+        case REQUEST_SEEDS:
+            return 0;
+        case DOWNLOAD_COMPLETE:
+            return 1;
+        case FINISHED:
+            return 2;
+        case UPDATE_SWARM:
+            return 3;
+        default:
+            return -1;
+    }
+}
+
+ClientRequest indexToRequest(int index) {
+    switch (index) {
+        case 0:
+            return ClientRequest::REQUEST_SEEDS;
+        case 1:
+            return ClientRequest::DOWNLOAD_COMPLETE;
+        case 2:
+            return ClientRequest::FINISHED;
+        case 3:
+            return ClientRequest::UPDATE_SWARM;
+        default:
+            return ClientRequest::REQUEST_SEEDS;
+    }
+}
